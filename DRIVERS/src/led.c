e9bf4f884fdd5717750d1f8b8e06f5631d255856
João Pedro Santos <145870334+santosjoaop@users.noreply.github.com>
2025-10-07 15:39:54 +0100

    Update led.c

--- DRIVERS/src/led.c
+++ DRIVERS/src/led.c
@@ -33,7 +33,7 @@ void LED_Init(bool state){
 /* Devolve true se o LED está aceso e false se o LED está apagado. */
 bool LED_GetState(void){
 
-	//if (){return true;}
+	if ((LPC_GPIO0 -> FIOPIN = (1<<P022)) == 1){return true;}
 
 	return false;
 }
@@ -52,8 +52,71 @@ void LED_Off(void){
 /* Troca o estado do LED. Se estiver aceso apaga o LED, caso contrario acende o LED. */
 void LED_Toggle(void){
 
-	LPC_GPIO0 -> FIOPIN ^= (1<<P022);
+	if ((LPC_GPIO0 -> FIOPIN = (1<<P022)) == 1){
+		LPC_GPIO0 -> FIOCLR = (1<<P022);
+	}else{
+		LPC_GPIO0 -> FIOSET = (1<<P022);
+	}
 }
 
+/*
+ * led.c
+ *
+ *  Created on: 07/10/2025
+ *      Author: diogo
+ */
+#ifdef __USE_CMSIS
+#include "LPC17xx.h"
+#endif
+#include "led.h"
 
+//DEFINES PARA CONTAS
+#define PCGPIO 15
+#define GPIO_FUNC 12
+#define P022 22
+
+
+/* Faz a iniciação do sistema para permitir a manipulação do estado LED do sistema de
+ * protopagem (LPCXPRESSO LPC1769), onde o parâmetro state se igual a false o LED
+ * fica apagado ou caso contrario fica aceso. */
+void LED_Init(bool state){
+
+    //garante que GPIO tem o clock ativo
+    LPC_SC->PCONP |= (1 << PCGPIO);
+
+    //garante que o pino esta no funcao de GPIO (00)
+    LPC_PINCON->PINSEL1 &= ~(3 << GPIO_FUNC );
+
+    LPC_GPIO0 -> FIODIR |= (1<<22);
+
+}
+
+/* Devolve true se o LED está aceso e false se o LED está apagado. */
+bool LED_GetState(void){
+
+	if ((LPC_GPIO0 -> FIOPIN = (1<<P022)) == 1){return true;}
+
+	return false;
+}
+/* Acende o LED. */
+void LED_On(void){
+
+	LPC_GPIO0 -> FIOSET = (1<<P022);
+}
+
+/* Apaga o LED. */
+void LED_Off(void){
+
+	LPC_GPIO0 -> FIOCLR = (1<<P022);
+}
+
+/* Troca o estado do LED. Se estiver aceso apaga o LED, caso contrario acende o LED. */
+void LED_Toggle(void){
+
+	if ((LPC_GPIO0 -> FIOPIN = (1<<P022)) == 1){
+		LPC_GPIO0 -> FIOCLR = (1<<P022);
+	}else{
+		LPC_GPIO0 -> FIOSET = (1<<P022);
+	}
+}
 
