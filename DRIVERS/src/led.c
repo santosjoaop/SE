787a066f08569730182cf0c191e6c64ee0eed418
João Pedro Santos <145870334+santosjoaop@users.noreply.github.com>
2025-10-07 15:18:29 +0100

    Update led.c

--- DRIVERS/src/led.c
+++ DRIVERS/src/led.c
@@ -4,18 +4,55 @@
  *  Created on: 07/10/2025
  *      Author: diogo
  */
+#ifdef __USE_CMSIS
+#include "LPC17xx.h"
+#endif
+#include "led.h"
+
+#define PCGPIO 15
+#define GPIO_FUNC 12
+#define P022 6
+
 
 /* Faz a iniciação do sistema para permitir a manipulação do estado LED do sistema de
  * protopagem (LPCXPRESSO LPC1769), onde o parâmetro state se igual a false o LED
  * fica apagado ou caso contrario fica aceso. */
-void LED_Init(bool state);
+void LED_Init(bool state){
+
+    //garante que GPIO tem o clock ativo
+    LPC_SC->PCONP |= (1 << PCGPIO);
+
+    //garante que o pino esta no funcao de GPIO (00)
+    LPC_PINCON->PINSEL1 &= ~(3 << GPIO_FUNC );
+
+    LPC_GPIO0 -> FIODIR |= (1<<22);
+
+}
+
 /* Devolve true se o LED está aceso e false se o LED está apagado. */
-bool LED_GetState(void);
+bool LED_GetState(void){
+
+	//if (){return true;}
+
+	return false;
+}
 /* Acende o LED. */
-void LED_On(void);
+void LED_On(void){
+
+	LPC_GPIO0 -> FIOSET2 = (1<<P022);
+}
+
 /* Apaga o LED. */
-void LED_Off(void);
+void LED_Off(void){
+
+	LPC_GPIO0 -> FIOCLR2 = (1<<P022);
+}
+
 /* Troca o estado do LED. Se estiver aceso apaga o LED, caso contrario acende o LED. */
-void LED_Toggle(void);
+void LED_Toggle(void){ //XOR
+
+	LPC_GPIO0 -> FIODIR ^= '1';
+}
+
 
 
