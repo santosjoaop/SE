1cc1ce07941f817ef2cdd7cbe3c6e8866c02f04d
João Pedro Santos <145870334+santosjoaop@users.noreply.github.com>
2025-10-13 15:25:21 +0100

    Update Lab2.c

--- Labs/Lab2.c
+++ Labs/Lab2.c
@@ -1,15 +1,3 @@
-/*
- * Copyright 2022 NXP
- * NXP confidential.
- * This software is owned or controlled by NXP and may only be used strictly
- * in accordance with the applicable license terms.  By expressly accepting
- * such terms or by downloading, installing, activating and/or otherwise using
- * the software, you are agreeing that you have read, and that you agree to
- * comply with and are bound by, such license terms.  If you do not agree to
- * be bound by the applicable license terms, then you may not retain, install,
- * activate or otherwise use the software.
- */
-
 #ifdef __USE_CMSIS
 #include "LPC17xx.h"
 #endif
@@ -17,10 +5,11 @@
 #include <cr_section_macros.h>
 
 #include <stdio.h>
-//#include "drivers.h"
+#include "led.h"
+#include "delay.h"
 
 
-#define BLINKING_TIME 5000
+#define BLINK_TIME 5000
 #define OFF_TIME 2000
 #define LED_PERIOD 500000 //0,5s = 500ms = 500000 us
 
@@ -28,17 +17,22 @@
 int main(void) {
     printf("Hello World\n");
 
-    while(){
-    	while(DELAY_GetElapsedMillis(BLINK_TIME) <= BLINK_TIME){	//Ou então podemos fazer toogle e delay???
+    LED_Init(true);
+    while(1){
+    	// Tempo atual
+    	int tempo_atual = DELAY_GetElapsedMillis(0);
+    	while(DELAY_GetElapsedMillis(tempo_atual) <= BLINK_TIME){	//Ou então podemos fazer toogle e delay???
+
     		LED_On();
     		DELAY_Microseconds(LED_PERIOD);
     		LED_Off();
     		DELAY_Microseconds(LED_PERIOD);
+
     	}
-    	while(DELAY_GetElapsedMillis(OFF_TIME) <= OFF_TIME){
+    	int tempo_passado = DELAY_GetElapsedMillis(0);
+    	while(DELAY_GetElapsedMillis(tempo_passado) <= OFF_TIME){
     		LED_Off();
     	}
-
 
     }
 
