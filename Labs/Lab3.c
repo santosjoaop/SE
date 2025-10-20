1a5904ceb3accc1b67f01f0ccf00673d4de87667
freixo2004 <119111566+freixo2004@users.noreply.github.com>
2025-10-20 14:11:45 +0100

    Add Lab3.c for LCD display functionality

---
+++ Labs/Lab3.c
@@ -0,0 +1,51 @@
+/**
+ * @file Lab3.c
+ * @author Grupo 2
+ * @brief
+ * @version 1.0
+ * @date 2025-10-19
+ *
+ * @copyright Copyright (c) 2025
+ * 
+ */
+#ifdef __USE_CMSIS
+#include "LPC17xx.h"
+#endif
+
+#include <cr_section_macros.h>
+
+#include <stdio.h>
+#include "delay.h"
+#include "lcd.h"
+
+
+#define SWITCH_LINES 1000											/**tempo de alternancia entre a primeira e segunda linha (em ms)*/
+#define ON_TIME 10000												/**tempo que o LCD fica a mostar (em ms)*/
+#define OFF_TIME 500												/**tempo que o LCD fica limpo (em ms)*/
+
+
+int main(void) {
+    printf("Hello World\n");
+
+    char string = {"TESTE LCD"};
+
+
+    DELAY_Init();
+    LCDText_Init();
+    while(1){
+    	int t_atual = DELAY_GetElapsedMillis(0);
+    	while(DELAY_GetElapsedMillis(t_atual) <= ON_TIME){
+
+    		LCDText_WriteString(string);
+    		DELAY_Milliseconds(SWITCH_LINES);
+    		//TROCA CURSOR POS
+
+    	}
+    	int t_passado = DELAY_GetElapsedMillis(0);
+    	while(DELAY_GetElapsedMillis(t_passado) <= OFF_TIME){
+    		LCDText_Clear();
+    	}
+
+    }
+
+}
