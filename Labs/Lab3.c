72866c80249676c08703be27d655d8818c36594f
João Pedro Santos <145870334+santosjoaop@users.noreply.github.com>
2025-10-27 16:23:50 +0000

    Fix string declaration and clean up LCD initialization

--- Labs/Lab3.c
+++ Labs/Lab3.c
@@ -27,20 +27,20 @@
 int main(void) {
     printf("Hello World\n");
 
-    char string = {"TESTE LCD"};
+    char* string = "TESTE LCD";
 
 
-    DELAY_Init();
-    LCDGPIO_Init(2,7);												/**Iniciamos os GPIO2 2..7*/
+    DELAY_Init();											/**Iniciamos os GPIO2 2..7*/
     LCDText_Init();
     while(1){
     	int t_atual = DELAY_GetElapsedMillis(0);
     	int line = 0;
     	while(DELAY_GetElapsedMillis(t_atual) <= ON_TIME){
 
+    		LCDText_Clear();
+    		LCDText_SetCursor(line, 0);
     		LCDText_WriteString(string);
     		DELAY_Milliseconds(SWITCH_LINES);
-    		LCDText_SetCursor(line, 0);
 
     		line = line ? 0 : 1;									/**mudar a linha*/
     	}
