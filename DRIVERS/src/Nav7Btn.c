aeee581b124735405fe059f64d2d20a94fac4589
freixo2004 <119111566+freixo2004@users.noreply.github.com>
2025-10-30 10:09:30 +0000

    Update Nav7Btn.c

--- DRIVERS/src/Nav7Btn.c
+++ DRIVERS/src/Nav7Btn.c
@@ -17,14 +17,16 @@
 
 #define PCGPIO (1 << 15)													/**MACRO ativar clock do GPIO*/
 #define GPIO_FUNC ~(3 << 12 )												/**MACRO mexer na função do PIN*/
+#define PULLDOWN 3
 
 
-#define P018 (1 << 18)														/**Col0*/
-#define P017 (1 << 17)														/**Col1*/
-#define P015 (1 << 15)														/**Row0*/
-#define P016 (1 << 16)														/**Row1*/
-#define P023 (1 << 23)														/**Row2*/
-#define P024 (1 << 24)														/**Row3*/
+
+#define COL1 (1 << 18)														/**P018*///IN
+#define COL2 (1 << 17)														/**P017*///IN
+#define ROW1 (1 << 15)														/**P015*/
+#define ROW2 (1 << 16)														/**P016*/
+#define ROW3 (1 << 23)														/**P023*/
+#define ROW4 (1 << 24)														/**P024*/
 
 
 
@@ -33,13 +35,27 @@
  */
 void NAVBTN_Init(void){
 
+	//GPIO CLOCK
 	LPC_SC->PCONP |= PCGPIO;
 
+	//FUNCTION GPIO para cada??pino ou chega o bloco
+	LPC_PINCON->PINSEL0 &= GPIO_FUNC;										/**Por causa do P015*/
 	LPC_PINCON->PINSEL1 &= GPIO_FUNC;
 
-	//PINMODE 0V or 5V
+	//OUTPUT PINS MODE PULL DOWN
+	LPC_PINCON->PINMODE0 = ~(PULLDOWN << 30);										/**Pin mode do P=15*/
+	LPC_PINCON->PINMODE1 = ~(PULLDOWN << 0);										/**Pin mode do P=16*/
+	LPC_PINCON->PINMODE1 = ~(PULLDOWN << 14);										/**Pin mode do P=23*/
+	LPC_PINCON->PINMODE1 = ~(PULLDOWN << 16);										/**Pin mode do P=24*/
 
-	return;
+
+	LPC_GPIO1 -> FIODIR |= COL1;
+	LPC_GPIO1 -> FIODIR |= COL2;
+	LPC_GPIO0 -> FIODIR |= ROW1;
+	LPC_GPIO1 -> FIODIR |= ROW2;
+	LPC_GPIO1 -> FIODIR |= ROW3;
+	LPC_GPIO1 -> FIODIR |= ROW4;
+
 }
 
 /**
@@ -47,22 +63,24 @@ void NAVBTN_Init(void){
  * @return devolve a primeira tecla detetada.Se nenhuma tecla estiver pressionada, devolve NAVBTN_NONE
  */
 NAVBTN_TypeDef NAVBTN_Read(void){
+	while(1){
 
-	if (((LPC_GPIO0 -> FIOPIN = P018) == 1) && ((LPC_GPIO0 -> FIOPIN = P017) == 0)){
-		if((LPC_GPIO0 -> FIOPIN = P015) == 1) return NAVBTN_UP;
-		if((LPC_GPIO0 -> FIOPIN = P016) == 1) return NAVBTN_DOWN;
-		if((LPC_GPIO0 -> FIOPIN = P023) == 1) return NAVBTN_LEFT;
-		if((LPC_GPIO0 -> FIOPIN = P024) == 1) return NAVBTN_RIGHT;
-		else return NAVBTN_NONE;
-	}
+		LPC_GPIO1 -> FIOCLR = COL2;
+		LPC_GPIO1 -> FIOSET = COL1;
+		if((LPC_GPIO1 -> FIOPIN = ROW2) == 1) return NAVBTN_BACK;
+		if((LPC_GPIO1 -> FIOPIN = ROW3) == 1) return NAVBTN_RIGHT;
+		if((LPC_GPIO1 -> FIOPIN = ROW4) == 1) return NAVBTN_ENTER;
 
-	if (((LPC_GPIO0 -> FIOPIN = P018) == 0) && ((LPC_GPIO0 -> FIOPIN = P017) == 1)){
-		if((LPC_GPIO0 -> FIOPIN = P015) == 1) return NAVBTN_CENTER;
-		if((LPC_GPIO0 -> FIOPIN = P016) == 1) return NAVBTN_BACK;
-		if((LPC_GPIO0 -> FIOPIN = P023) == 1) return NAVBTN_ENTER;
-		else return NAVBTN_NONE;
+
+		LPC_GPIO1 -> FIOCLR = COL1;
+		LPC_GPIO1 -> FIOSET = COL2;
+		if((LPC_GPIO0 -> FIOPIN = ROW1) == 1) return NAVBTN_UP;
+		if((LPC_GPIO1 -> FIOPIN = ROW2) == 1) return NAVBTN_DOWN;
+		if((LPC_GPIO1 -> FIOPIN = ROW3) == 1) return NAVBTN_LEFT;
+		if((LPC_GPIO1 -> FIOPIN = ROW4) == 1) return NAVBTN_CENTER;
+
+		return NAVBTN_NONE;
 	}
-	return NAVBTN_NONE;
 }
 
 /**
@@ -75,8 +93,8 @@ NAVBTN_TypeDef NAVBTN_Pressed(void){
 
 	NAVBTN_TypeDef before = NAVBTN_Read();
 	DELAY_Microseconds(100);
-	NAVBTN_TypeDef now;
-	while(now == NAVBTN_Read()){
+	NAVBTN_TypeDef now = NAVBTN_Read();
+	while(before == now ){
 		return NAVBTN_NONE;
 	}
 	return now;
