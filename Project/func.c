2aec208c934184ae0f2985b739b04f6b19681cb1
freixo2004 <119111566+freixo2004@users.noreply.github.com>
2025-11-11 15:05:05 +0000

    Refactor LCDTime_Print and add ValidDate function

--- Project/func.c
+++ Project/func.c
@@ -17,11 +17,25 @@
 #include "rtc.h"
 
 
-//define
 
 
-void LCDTime_Print(struct tm data){
-	LCDText_Printf("%02d:%02d %02d/%02d/%04d",data.tm_hour,data.tm_min,data.tm_mday,data.tm_mon + 1,data.tm_year + 1900);
+void LCDTime_Print(struct tm* data, int BlinkField){
+
+	LCDText_Printf("%02d:%02d %02d/%02d/%04d",data->tm_hour,data->tm_min,data->tm_mday,data->tm_mon + 1,data->tm_year + 1900);
+}
+
+int ValidDate(struct tm* data){
+	int year = data->tm_year + 1900;
+
+	int daysInMonth[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+	if(data->tm_mon + 1 == 2){
+		if ((year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0))) daysInMonth[1] = 29;
+	}
+
+	if(data->tm_mday > daysInMonth[data->tm_mon]) return 0;
+
+	return 1;
 }
 
 
@@ -30,31 +44,36 @@ int ChangeTime(struct tm* data ,int field, int a){
 	switch(field){
 		case 0:
 			newVal = data->tm_hour + (a > 0 ? 1 : -1);
-			if(newVal < 0 || newVal > 23) return -1;
+			if(newVal < 0) newVal = 23;
+			if(newVal > 23)	newVal = 0;
 			data->tm_hour = newVal;
 			return 0;
 
 		case 1:
 			newVal = data->tm_min + (a > 0 ? 1 : -1);
-			if(newVal < 0 || newVal > 59) return -1;
+			if(newVal < 0) 	newVal = 59;
+			if(newVal > 59)	newVal = 0;
 			data->tm_min = newVal;
 			return 0;
 
 		case 2:
 			newVal = data->tm_mday + (a > 0 ? 1 : -1);
-			if(newVal <= 0 || newVal > 31) return -2;			//dias num mês, bissestos etc
+			if(newVal <= 0) newVal = 31;
+			if(newVal > 31)	newVal = 1;
 			data->tm_mday = newVal;
 			return 0;
 
 		case 3:
 			newVal = data->tm_mon + (a > 0 ? 1 : -1);
-			if(newVal < 0 || newVal > 11) return -1;
+			if(newVal < 0) 	newVal = 11;
+			if(newVal > 11)	newVal = 0;
 			data->tm_mon = newVal;
 			return 0;
 
 		case 4:
 			newVal = data->tm_year + (a > 0 ? 1 : -1);
-			if(newVal < 1970 || newVal > 4096) return -1;
+			if(newVal < 0) 	 newVal = 4095 - 1900;
+			if(newVal > 4095)newVal = 0 - 1900;
 			data->tm_year = newVal;
 			return 0;
 
@@ -72,8 +91,9 @@ void Inits(void){
     NAVBTN_Init();
     DELAY_Init();
     LCDText_Init();
-    RTC_Init(time(NULL));						//apenas funciona com pc ligago
-    ///RTC_Init(1762180178);						//independente
+    ///RTC_Init(time(NULL));						//apenas funciona com pc ligago
+    //RTC_Init(1762180178);						//independente
+    RTC_Init(0);
 }
 
 
@@ -89,7 +109,7 @@ void Operation_MODE(void){
 			LPC_RTC->ILR |= (1 << 0); 			//clear ao registo de incremento
 			RTC_GetTimeDate(&now);
 			LCDText_Clear();
-			LCDTime_Print(now);
+			LCDTime_Print(&now, 0);
 		 }
 
 		switch(NAVBTN_Read()){					//Leitura do butão
@@ -129,7 +149,7 @@ void Operation_MODE(void){
 }
 
 
-void Config_MODE(void){
+int Config_MODE(void){
 	int field = 0;
 	struct tm saved;							//guarda a hora e data do RTC a posta
 	RTC_GetTimeDate(&saved);
@@ -158,21 +178,22 @@ void Config_MODE(void){
 
 			case NAVBTN_BACK:
 				//não altera registos do RTC
-				return;
+				return 0;
 
 			case NAVBTN_ENTER:
-				//verificar se saved é valida				//se for uma hora e data válida faz set no RTC
+				if(!ValidDate(&saved))return -1;//verificar se saved é valida				//se for uma hora e data válida faz set no RTC
 				RTC_SetTimeDate(&saved);					//afeta os registos do RTC com o valor das alterações
-				return;
+				return 1;
 
 			default:
 				break;
 		}
 
 		LCDText_Clear();
-		LCDTime_Print(saved);								//Blink param!!!!!!!!!!!!!!!
+		LCDTime_Print(&saved, 0);								//Blink param!!!!!!!!!!!!!!!
 		DELAY_Milliseconds(150);
 	}
+	return -2;
 }
 
 
