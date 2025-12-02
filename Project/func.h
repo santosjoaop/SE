b39d12483d2b42b14ca56e8cad2c28cf33106ec1
freixo2004 <119111566+freixo2004@users.noreply.github.com>
2025-12-02 21:31:05 +0000

    Modify func.h to include time.h and new functions
    Updated function declarations and added new LCD_Cover function.

--- Project/func.h
+++ Project/func.h
@@ -23,29 +23,39 @@
  *
  * @{
  */
-#include <stdbool.h>
+#include <time.h>
 
 
 
-void LCDTime_Print(struct tm* data, int BlinkField);
+/**
+ * @brief Função gene´rica para fazer covers
+ * str string para a a cover
+ * time tempo em ms a mostrar a cover
+ */
+void LCD_Cover(char* str, int time);
 
+//Print do ecrã com hora, data e a frequencia atual
+void LCD_Time(struct tm* data, float freq);
+
+//função que põe o parametro a ser alterado no momento a piscar
+void LCD_Time_Blink(struct tm* data, int BlinkField, int changes);
+
+//print do volume
 void LCDVolume_Print(int volume);
 
-
-
+//Verifica se a data a ser guardada é válida
 int ValidDate(struct tm* data);
 
-int ChangeTime(struct tm* data ,int field, int a);
+//Altera os campos da data/hora no modo de configuração
+int ChangeTime(struct tm* data, int field, int a);
 
 
 
 void Inits(void);
-
+void SetVolume_MODE(int* volume);
 void Operation_MODE(void);
-
 int Menu_MODE(void);
-
 int Config_MODE(void);
 
 
-#endif /* _NAV7BTN_H_ */
+#endif /* _FUNC_H_ */
