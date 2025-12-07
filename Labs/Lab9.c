f42dca3c0d6f07ffb378099ef605a7d800a3a978
freixo2004 <119111566+freixo2004@users.noreply.github.com>
2025-12-07 10:57:46 +0000

    DEbug_Lab9_Final

--- Labs/Lab9.c
+++ Labs/Lab9.c
@@ -2,7 +2,7 @@
  * @file Lab9.c
  * @author Grupo 2
  * @brief
- * @version 2.0
+ * @version 1.0
  * @date 2025-12-04
  *
  * @copyright Copyright (c) 2025
@@ -22,25 +22,40 @@
 #include "radio.h"
 
 
+
 int main(void){
-    DELAY_Init();
+	uint16_t data;
+
+	DELAY_Init();
     LCDText_Init();
-
-    Radio_Init();
-
-    LCDText_Printf("RDA5807 Test");
+    Radio_Init();								//radio valores default
 
 
-    uint16_t data;
-    while(1){
-        DELAY_Milliseconds(500);
+    Radio_SHUTDOWN(0);
 
-        //Radio_Write_Word(0x05, 0x0000);
-        Radio_Write_Bits(0x05, 10, 7, 4);
+    Radio_Write_Word(0x02, 0xC001);
+    Radio_Read_Word(0x02 , &data);
+    LCDText_Clear();
+    LCDText_Printf("\nREG2: 0x%04X", data);
+    DELAY_Milliseconds(500);
 
 
 
-        Radio_Read_Word(0x05 , &data);
-        LCDText_Printf("\nREG05: 0x%04X", data);
-    }
+    Radio_Write_Word(0x03, 0x3110);
+    Radio_Read_Word(0x03 , &data);
+    LCDText_Clear();
+    LCDText_Printf("\nREG3: 0x%04X", data);
+    DELAY_Milliseconds(500);
+
+
+
+    Radio_Write_Word(0x05, 0x0015);
+    Radio_Read_Word(0x03 , &data);
+    LCDText_Clear();
+    LCDText_Printf("\nREG5: 0x%04X", data);
+    DELAY_Milliseconds(500);
+
+
+    LCDText_Clear();
+    LCDText_Printf("RFM\n106.6 MHz");
 }
