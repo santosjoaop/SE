7ba8c6ae5998b9652ccd9b68a887e576efee63cc
freixo2004 <119111566+freixo2004@users.noreply.github.com>
2025-12-07 11:20:18 +0000

    Create radio.c

---
+++ DRIVERS/src/radio.c
@@ -0,0 +1,152 @@
+/**
+ * @file radio.c
+ * @author Grupo 2
+ * @brief
+ * @version 1.0
+ * @date 2025-12-04
+ *
+ * @copyright Copyright (c) 2025
+ * 
+ */
+#ifdef __USE_CMSIS
+#include "LPC17xx.h"
+#endif
+#include "delay.h"
+#include "lcd.h"
+#include "i2c.h"
+#include "radio.h"
+#include <stdio.h>
+
+
+#define P021 21
+#define PCGPIO 15
+#define GPIO_FUNC 10
+
+
+
+
+////////////////////////////////////////////////////////////////
+static uint16_t make_mask(int e, int s) {
+    uint16_t mask = 0;
+
+    for(int i = s; i <= e; i++){
+        mask |= (1u << i);
+    }
+
+    return mask;
+}
+
+
+static uint16_t change_bits(uint16_t copy, uint16_t data, int end, int start){
+	uint16_t shifted_data = (data << start) & make_mask(end, start);
+
+	// Replace bits one-by-one
+	for (int i = start; i <= end; i++) {
+		uint16_t bit_from_data = shifted_data & (1u << i);
+
+		if(bit_from_data) 	copy |= (1u << i);
+		else 				copy &= ~(1u << i);
+
+	}
+
+	 return copy;
+}
+
+
+////////////////////////////////////////////////////////////////
+int Radio_Init(void){
+	I2CMASTER_Init();
+	I2CMASTER_SetFrequency(100000);
+
+	//Register Definition Default Values
+	/*
+	Radio_Write_Word(0x02,0x0000);
+	Radio_Write_Word(0x03,0x13F0);
+	Radio_Write_Word(0x04,0x0000);
+	Radio_Write_Word(0x05,0x888B);
+	Radio_Write_Word(0x06,0x0000);
+	Radio_Write_Word(0x07,0x1082);
+	Radio_Write_Word(0x08,0x0000);
+	*/
+
+	return 0;
+}
+
+
+
+int Radio_Write_Word(uint8_t reg, uint16_t data){
+	uint8_t writeBuf[3];
+	writeBuf[0] = reg;    					//valor hexa do registo
+	writeBuf[1] = (data >> 8) & 0xFF;  		//2ºbit do data(parte alta)
+	writeBuf[2] = data & 0xFF;				//1ºbit do data(parte baixa)
+
+	if (I2CMASTER_Transmit(0x11, writeBuf, 3) != 0) {
+		LCDText_Printf("Write Error");
+		return -1;
+	}
+
+	return 0;
+}
+
+
+int Radio_Read_Word(uint8_t reg, uint16_t* data){
+	uint8_t readBuf[2];
+
+	if(I2CMASTER_Transmit(0x11, &reg, 1) != 0){
+		LCDText_Printf("\nTX Error");
+		return -1;
+	}
+	if(I2CMASTER_Receive(0x11, readBuf, 2) != 0){
+		LCDText_Printf("\nRX Error");
+		return -2;
+	}
+
+	 *data = (readBuf[0] << 8) | readBuf[1];
+
+	 return 0;
+}
+
+
+
+int Radio_Write_Bits(uint8_t reg, uint16_t data, int end, int start){
+	uint16_t reg_copy;
+	Radio_Read_Word(reg, &reg_copy);
+
+	uint16_t reg_copy_new;
+	reg_copy_new = change_bits(reg_copy, data, end, start);
+
+	Radio_Write_Word(reg, reg_copy_new);
+
+	return 0;
+}
+
+
+int Radio_SetVolume(int volume){
+	if(volume == 0) Radio_SHUTDOWN(1);
+	else Radio_SHUTDOWN(0);
+
+	Radio_Write_Bits(0x05, volume, 3, 0);
+	return 0;
+}
+
+int Radio_SetFreq(float freq){
+
+	return 0;
+}
+
+
+int Radio_SHUTDOWN(int on){
+	LPC_SC->PCONP |= (1 << PCGPIO);
+
+	LPC_PINCON->PINSEL1 &= ~(3 << GPIO_FUNC );
+
+	LPC_GPIO0 -> FIODIR |= (1<<P021);
+
+	if(on) 	LPC_GPIO0 -> FIOSET = (1<<P021); //LPC_GPIO0 -> FIOSET |= (1<<22)
+	else 	LPC_GPIO0 -> FIOCLR = (1<<P021);
+
+	return on;
+}
+
+
+
