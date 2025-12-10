4f0e22550c8ca4d463793e8822a50444478356e5
freixo2004 <119111566+freixo2004@users.noreply.github.com>
2025-12-10 11:00:20 +0000

    STILL NOT WORKING

--- Labs/Lab9.c
+++ Labs/Lab9.c
@@ -11,9 +11,7 @@
 #ifdef __USE_CMSIS
 #include "LPC17xx.h"
 #endif
-
 #include <cr_section_macros.h>
-
 #include <stdio.h>
 #include <string.h>
 #include "delay.h"
@@ -24,38 +22,25 @@
 
 
 int main(void){
-	uint16_t data;
-
-	DELAY_Init();
+    uint16_t data;
+    DELAY_Init();
     LCDText_Init();
-    Radio_Init();								//radio valores default
-
+    Radio_Init();
 
     Radio_SHUTDOWN(0);
 
-    Radio_Write_Word(0x02, 0xC001);
-    Radio_Read_Word(0x02 , &data);
-    LCDText_Clear();
-    LCDText_Printf("\nREG2: 0x%04X", data);
-    DELAY_Milliseconds(500);
-
-
-
-    Radio_Write_Word(0x03, 0x3110);
-    Radio_Read_Word(0x03 , &data);
-    LCDText_Clear();
-    LCDText_Printf("\nREG3: 0x%04X", data);
-    DELAY_Milliseconds(500);
-
-
-
-    Radio_Write_Word(0x05, 0x0015);
-    Radio_Read_Word(0x03 , &data);
-    LCDText_Clear();
-    LCDText_Printf("\nREG5: 0x%04X", data);
-    DELAY_Milliseconds(500);
-
+    Radio_Write_Word(0x02, 0xC001);  // ENABLE + UNMUTE
+    Radio_Write_Word(0x03, 0x9C40);  // tune 98.3 MHz
+    DELAY_Milliseconds(200);         // wait for STC
+    Radio_Write_Word(0x03, 0x1C40);  // clear TUNE bit
+    Radio_Write_Word(0x05, 0x001F);  // max volume
 
     LCDText_Clear();
-    LCDText_Printf("RFM\n106.6 MHz");
+    LCDText_Printf("Antena 1\n98.3MHz");
+
+    // READ STATUS REGISTER (NOT REG02)
+    Radio_Read_Word(0x0A , &data);
+    LCDText_Clear();
+    LCDText_Printf("\nREG0A: 0x%04X", data);
 }
+
