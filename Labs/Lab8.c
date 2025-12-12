a94a1635c65e7abf21e74dad9700da604180e871
freixo2004 <119111566+freixo2004@users.noreply.github.com>
2025-12-12 10:32:19 +0000

    Update Lab8.c

--- Labs/Lab8.c
+++ Labs/Lab8.c
@@ -1,13 +1,17 @@
 /**
  * @file Lab8.c
- */
-
+ * @author Grupo 2
+ * @brief
+ * @version 1.0
+ * @date 2025-12-04
+ *
+ * @copyright Copyright (c) 2025
+ * 
+ */
 #ifdef __USE_CMSIS
 #include "LPC17xx.h"
 #endif
-
 #include <cr_section_macros.h>
-
 #include <stdio.h>
 #include <string.h>
 #include "delay.h"
@@ -15,33 +19,31 @@
 #include "i2c.h"
 
 
-int main(void)
-{
+
+int main(void){
     DELAY_Init();
     LCDText_Init();
 
     I2CMASTER_Init();
-    I2CMASTER_SetFrequency(100000);   // 100 kHz I2C
+    I2CMASTER_SetFrequency(100000);
 
     LCDText_Printf("RDA5807 Test");
 
-    uint8_t reg = 0x00;   // register 0
+    uint8_t reg = 0x05;
     uint8_t buf[2];
 
     while(1){
 		DELAY_Milliseconds(500);
-		// STEP 1: Write register address (0x00)
 		if(I2CMASTER_Transmit(0x11, &reg, 1) != 0){
 			LCDText_Printf("\nTX Error     ");
 			continue;
 		}
-		// STEP 2: Read 2 bytes from RDA5807
 		if(I2CMASTER_Receive(0x11, buf, 2) != 0){
 			LCDText_Printf("\nRX Error     ");
 			continue;
 		}
-		// Combine bytes into 16-bit value
 		uint16_t id = (buf[0] << 8) | buf[1];
-		LCDText_Printf("\nID: 0x%04X   ", id);
+		LCDText_Printf("\nREG5: 0x%04X   ", id);
     }
 }
+
